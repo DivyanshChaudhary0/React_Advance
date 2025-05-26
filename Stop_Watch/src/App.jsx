25312321c56315a76a7048f0a506a0bad49727a1
Divyansh chaudhary <hypr123890@gmail.com>
2025-05-26 14:29:31 +0530

    runTimer and handlePause functon done

--- Stop_Watch/src/App.jsx
+++ Stop_Watch/src/App.jsx
@@ -6,23 +6,44 @@ function App() {
   const [minute, setMinute] = useState(0);
   const [second, setSecond] = useState(0);
   const timer = useRef(null);
+  const [isStarted, setIsStarted] = useState(false);
+  const [isPaused, setIsPaused] = useState(false);
+
+  function handleStart(){
+    setIsStarted(true);
+  }
+
+  function runTimer(hour,minute,second){
+    console.log("timerStarts");
+  }
+
+  function handlePause(){
+    setIsPaused(true);
+  }
 
   useEffect(() => {
+    if(!isStarted) return;
+    timer.current = setInterval(function(){
+      runTimer(hour,minute,second)
+    },1000)
 
-  },[])
+    return () => {
+      clearTimeout(timer.current);
+    }
+  },[isStarted])
 
   return (
     <div className='container'>
       <div className='timer'>
-        <p>Hours: <span className='time'>00</span></p>
-        <p>Minutes: <span className='time'>00</span></p>
-        <p>Seconds: <span className='time'>00</span></p>
+        <p>Hours: <span className='time'>{hour < 10 ? `0${hour}`: hour}</span></p>
+        <p>Minutes: <span className='time'>{minute < 10 ? `0${minute}`: minute}</span></p>
+        <p>Seconds: <span className='time'>{second < 10 ? `0${second}`: second}</span></p>
       </div>
 
       <div>
-        <button>Start</button>
-        <button>Pause</button>
-        <button>Reset</button>
+        {!isStarted && <button onClick={handleStart}>Start</button>}
+        {isStarted && <button onClick={handlePause}>Pause</button>}
+        {isStarted && <button onClick={handleReset}>Reset</button>}
       </div>
 
     </div>
