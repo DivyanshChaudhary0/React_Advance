619fa63da205b48349124165802edcbcd781b0ce
Divyansh chaudhary <hypr123890@gmail.com>
2025-06-11 14:54:34 +0530

    feat: enhance ShowExpense component to display expenses in a table format

--- Expense_Tracker/src/ShowExpense.jsx
+++ Expense_Tracker/src/ShowExpense.jsx
@@ -1,10 +1,34 @@
-
-import React from 'react'
-
-const ShowExpense = () => {
+const ShowExpense = ({ allExpenses }) => {
   return (
-    <div>ShowExpense</div>
-  )
-}
+    <table border="1" cellpadding="8" cellspacing="0">
+      <thead>
+        <tr>
+          <th>Expense Name</th>
+          <th>Expense Value (₹)</th>
+        </tr>
+      </thead>
 
-export default ShowExpense
+      <tbody>
+        {
+            allExpenses.length > 0 && allExpenses.map((val) => {
+                return(
+                    <tr>
+                        <td>{val?.expenseName}</td>
+                        <td>{val?.expenseValue}</td>
+                    </tr>
+                )
+            })
+        }
+      </tbody>
+
+      <tfoot>
+        <tr>
+          <th>Total</th>
+          <th>₹5500</th>
+        </tr>
+      </tfoot>
+    </table>
+  );
+};
+
+export default ShowExpense;
